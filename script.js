2d9e4f2be0963df108e26181bb32b6b4446b32e3
naim195 <naimkhan.shaikh@iitgn.ac.in>
2023-12-31 02:53:10 +0530

    html css done starting with js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let calcBody=document.querySelector('.calc-body');
 let btnNos=document.querySelector('#button-nos');
 let btnOps=document.querySelector('#button-ops');
+let button=document.createElement('div');
 
+const buttonOrder=[7, 8, 9, 4, 5, 6, 1, 2, 3, 0,'.']
 function createButtonsNos(){    
-    for(let i=9;i>=0;i--){
+    for(let i=0;i<buttonOrder.length;i++){
         let button=document.createElement('div');
         button.className='button no';
-        button.innerText=i;
+        button.innerText=buttonOrder[i];
+        if(i==buttonOrder.length-2) button.className+=' zero';
         btnNos.appendChild(button);
     }
     
@@ -21,6 +24,24 @@ function createButtonsOps(){
         btnOps.appendChild(button);
     }
 }
+
+function add(a,b){
+    return a+b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+
 
 createButtonsNos();
 createButtonsOps();
